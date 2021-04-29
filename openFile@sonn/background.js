0bcf63255aca7583115bed19fe29169ebfdd26b0
Markus Eberl <eberl@sonn.at>
2021-04-29 15:24:56 +0200

    openFile@sonn.at 2.0-rc.1    * add LegacyPref for thunderbird.cfg read    * refactoring

--- openFile@sonn/background.js
+++ openFile@sonn/background.js
@@ -2,57 +2,41 @@ browser.menus.create({
     id: "phoenixqs-OpenFile",
     title: "Akt öffnen Alt+Strg+D",
     contexts: ["message_list"],
-    async onclick() {SONN_openFile();}
+    async onclick() {await SONN_openFile();}
 });
 
-browser.commands.onCommand.addListener((command) => {
-    if (command === "Event-SONN_openFile") {
-        SONN_openFile();
-    }
+browser.commands.onCommand.addListener(async () => {
+    await SONN_openFile();
 });
 
 browser.messageDisplayAction.onClicked.addListener(async () => {
-    SONN_openFile();
+    await SONN_openFile();
 });
 
-function SONN_openFile() {
-    browser.tabs.query({
-        active: true,
-        currentWindow: true,
-    }).then(tabs => {
-        let tabId = tabs[0].id;
-        browser.messageDisplay.getDisplayedMessage(tabId).then((message) => {
-            //browser.OpenFolder.showItemInFolder("/opt/");
-            let dirtypes = {e: 100, g: 100, j: 100, k: 100, m: 500, r: 500, s: 100, u: 100};
+async function SONN_openFile() {
+    let tabs = await browser.tabs.query({active: true, currentWindow: true,})
+    let tabId = tabs[0].id;
+    let message = await browser.messageDisplay.getDisplayedMessage(tabId);
+    //browser.OpenFolder.showItemInFolder("/opt/");
+    let dirtypes = {e: 100, g: 100, j: 100, k: 100, m: 500, r: 500, s: 100, u: 100};
 
-            // Try to use file number for lookup
-            let subject = message.subject;
-            console.log(subject);
-            let file_match = subject.match(/\b([RSGMUEJK] ?\d{4,5}|S ?\d{3})\b/gi);
+    // Try to use file number for lookup
+    let subject = message.subject;
+    let file_match = subject.match(/\b([RSGMUEJK] ?\d{4,5}|S ?\d{3})\b/gi);
+    if (file_match == null) {return}
 
-            if (file_match) {
-                var filebase = "";
-                try {
-                    // TODO filebase in cfg or policies.json
-                    filebase = "/poseidon/akten/"
-                } catch (ex) {
-                    // do nothing
-                }
-                if (filebase) {
-                    for (let m of file_match) {
-                        f = m.toLowerCase().replace(/ /g, '');
-                        filetype = f.slice(0,1);
-                        filenum = parseInt(f.slice(1), 10);
-                        dirnum = filenum - filenum % dirtypes[filetype];
-                        fileurl = filebase + filetype + "/" +
-                            dirnum.toString().padStart(5, '0') + "-" +
-                            (dirnum + dirtypes[filetype] - 1).toString().padStart(5, '0') +
-                            "/" + filetype + filenum.toString().padStart(5, '0');
-                        console.log(fileurl);
-                        browser.OpenFolder.showItemInFolder(fileurl);
-                    }
-                }
-            }
-        })
-    })
+    let filebase = await messenger.LegacyPrefs.getPref("extensions.phoenixqs.filebase");
+    if (filebase == null) {return}
+
+    for (let m of file_match) {
+        let f = m.toLowerCase().replace(/ /g, '');
+        let filetype = f.slice(0, 1);
+        let filenum = parseInt(f.slice(1), 10);
+        let dirnum = filenum - filenum % dirtypes[filetype];
+        let fileurl = filebase + filetype + "/" +
+            dirnum.toString().padStart(5, '0') + "-" +
+            (dirnum + dirtypes[filetype] - 1).toString().padStart(5, '0') +
+            "/" + filetype + filenum.toString().padStart(5, '0');
+        await browser.OpenFolder.showItemInFolder(fileurl);
+    }
 }
--- openFile@sonn/notes.md
+++ openFile@sonn/notes.md
@@ -1,3 +1,5 @@
+openFolderApi: https://thunderbird.topicbox.com/groups/addons/Tfb76bff3d15a2a97-M3000f2aa425418f8f5aa6eaf/launch-external-url
+
 bugs:
    * sendRemoveListener on closed conduit openFile@sonn.309
 https://bugzilla.mozilla.org/show_bug.cgi?id=1633315
