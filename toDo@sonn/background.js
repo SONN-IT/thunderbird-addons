192f1a4231904a6e5038f70a9f6fadd8beea73fb
Markus Eberl <eberl@sonn.at>
2021-12-30 08:52:04 +0100

    toDo@sonn.at 1.0-rc1

---
+++ toDo@sonn/background.js
@@ -0,0 +1,42 @@
+browser.messageDisplayAction.onClicked.addListener(async () => {
+    await main();
+});
+
+async function main() {
+    let tabs = await browser.tabs.query({active: true, currentWindow: true});
+    console.log("tabs: ", tabs);
+    let tabId = tabs[0].id;
+    let message = await browser.messageDisplay.getDisplayedMessage(tabId);
+    console.log("message: ", message);
+    if (!message) {return 0}
+
+    if (message.folder.path === "/Drafts") {
+        // TODO: SIN would delete a draft?
+        console.log("mail in drafts folder")
+        let imapUid = await browser.ImapTools.getImapUID(message.id);
+        browser.windows.openDefaultBrowser("http://localhost/" + encodeURIComponent(imapUid));
+    } else {
+        browser.messages.onCopied.addListener(async function copiedMessageListener(originalMessages, copiedMessages) {
+            console.log("copiedMessageListener");
+            let originalMessage = originalMessages.messages[0];
+            let copiedMessage = copiedMessages.messages[0];
+
+            if (originalMessage.id === message.id && copiedMessage) {
+                let imapUid = await browser.ImapTools.getImapUID(copiedMessage.id)
+                console.log("copiedMessage imapUid", imapUid);
+                console.log("copiedMessage: ", copiedMessage);
+                browser.windows.openDefaultBrowser("http://localhost/" + encodeURIComponent(imapUid));
+            }
+            browser.messages.onCopied.removeListener(copiedMessageListener);
+        });
+
+        try {
+            await browser.messages.copy([message.id], {
+                accountId: message.folder.accountId,
+                path: '/Drafts'
+            });
+        } catch (e) {
+            console.log("Failed to copy message: ", e);
+        }
+    }
+}
