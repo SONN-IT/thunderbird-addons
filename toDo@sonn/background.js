cafe8457cfda5333d61daa70e1efc7e4a87d2ace
Markus Eberl <eberl@sonn.at>
2022-12-13 12:38:23 +0100

    toDo@sonn.at: 1.0-rc6

--- toDo@sonn/background.js
+++ toDo@sonn/background.js
@@ -3,15 +3,15 @@ browser.messageDisplayAction.onClicked.addListener(async () => {
 });
 
 async function main() {
-    let tabs = await browser.tabs.query({active: true, currentWindow: true});
-    console.log("tabs: ", tabs);
-    let tabId = tabs[0].id;
-    let message = await browser.messageDisplay.getDisplayedMessage(tabId);
-    console.log("message: ", message);
+    const tabs = await browser.tabs.query({active: true, currentWindow: true});
+    // console.log("tabs: ", tabs);
+    const tabId = tabs[0].id;
+    const message = await browser.messageDisplay.getDisplayedMessage(tabId);
+    // console.log("message: ", message);
     if (!message) {return 0}
 
     if (message.folder.path === "/Drafts") {
-        console.log("drafts mail: exit")
+        // console.log("drafts mail: exit")
         browser.notifications.create({
             "type": "basic",
             "title": "Thunderbird - ToDo",
@@ -19,23 +19,27 @@ async function main() {
         });
     } else {
         browser.messages.onCopied.addListener(async function copiedMessageListener(originalMessages, copiedMessages) {
-            console.log("copiedMessageListener");
-            let originalMessage = originalMessages.messages[0];
-            let copiedMessage = copiedMessages.messages[0];
+            // console.log("copiedMessageListener");
+            const originalMessage = originalMessages.messages[0];
+            const copiedMessage = copiedMessages.messages[0];
 
             if (originalMessage.id === message.id && copiedMessage) {
-                let imapUid = await browser.ImapTools.getImapUID(copiedMessage.id)
-                console.log("copiedMessage imapUid", imapUid);
-                console.log("copiedMessage: ", copiedMessage);
-                let baseUrl = await messenger.LegacyPrefs.getPref("extensions.todo.baseurl");
+                const headerMessageId = copiedMessage.headerMessageId
+                // console.log("copiedMessage imapUid", headerMessageId);
+                // console.log("copiedMessage: ", copiedMessage);
+                const baseUrl = await messenger.LegacyPrefs.getPref("extensions.todo.baseurl");
                 if (baseUrl == null) {
-                    console.log("please setup pref extensions.todo.baseurl e.g. https://localhost");
+                    // console.log("please setup pref extensions.todo.baseurl e.g. https://localhost");
+                    browser.notifications.create({
+                        "type": "basic",
+                        "title": "Thunderbird - ToDo",
+                        "message": "please setup pref extensions.todo.baseurl e.g. https://localhost"
+                    });
                     return
                 }
 
-                if(imapUid) {
-                    // let query = "?uid=" + imapUid + ";subject=" + encodeURIComponent(copiedMessage.subject);
-                    let query = "msgID=" + imapUid.toString();
+                if(headerMessageId) {
+                    const query = "msgID=" + headerMessageId.toString();
                     browser.windows.openDefaultBrowser(baseUrl + "?" + query);
                 } else {
                     browser.notifications.create({
@@ -54,7 +58,12 @@ async function main() {
                 path: '/Drafts'
             });
         } catch (e) {
-            console.log("Failed to copy message: ", e);
+            // console.log("Failed to copy message: ", e);
+            browser.notifications.create({
+                "type": "basic",
+                "title": "Thunderbird - ToDo",
+                "message": "Die E-Mail konnte nicht kopiert werden"
+            });
         }
     }
 }
